3f8fae55a21c6a9015a9947cd37bd823349e6272
801341 <801341@users.noreply.github.com>
2019-09-16 10:29:08 -0700

    nested loops

---
+++ APCSA/lab916nestedloops/NestedForTri.java
@@ -0,0 +1,33 @@
+//© A+ Computer Science
+// www.apluscompsci.com
+
+//nested for loop example
+
+public class NestedForTri
+{
+   public static void main(String args[])
+   {
+
+   	/*
+   	 	        *
+			**
+			***
+			****
+			*****
+			******
+			*******
+			********
+			*********
+		*/
+
+		int stop=9;
+		//add in a for loop to print the rows   //rows
+		for (int outer = 1; outer <=stop; i++){
+		    for (int inner = outer; i
+		   //add in a for loop to print out the * on each row  //columns
+		      System.out.print("*");
+		   //print out a new line
+
+
+	}
+}
