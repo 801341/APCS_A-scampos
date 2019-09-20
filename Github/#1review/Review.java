483b530e317681d35256352a6ceaa6d172b3fabf
801341 <801341@users.noreply.github.com>
2019-09-20 10:28:23 -0700

    github commit

---
+++ Github/#1review/Review.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Review here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Review
+{
+    //a. false
+    //boolean test = true
+    //c. false
+    //d. int val = 7/12
+}
