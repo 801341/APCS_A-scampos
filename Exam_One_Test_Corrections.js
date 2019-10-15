d9a145c732a03c9fe308d8c028f444572c37a56c
801341 <54371464+801341@users.noreply.github.com>
2019-10-15 01:40:02 -0700

    Update Exam_One_Test_Corrections.js

--- Exam_One_Test_Corrections.js
+++ Exam_One_Test_Corrections.js
@@ -1,1 +1,24 @@
-
+/**
+ * Number 10
+ *
+ * @author (Stephanie)
+ * @version (10/14/19)
+ */
+public class pumpkin
+{
+    private int num;
+    public pumpkin(int x)
+    {
+        num = x;
+    }
+    public static int digitsSum(int[] num)
+    {
+    int i = 0;
+    int sum = 0;
+    while(i < num.length){
+        sum += num[i];
+        i++;
+    }
+    return sum;
+    }
+}
