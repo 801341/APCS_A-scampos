d77ed4fc84c00dcc82eb0854273b226c0608ff61
801341 <54371464+801341@users.noreply.github.com>
2019-10-17 13:39:55 -0700

    Update Exam_One_Test_Corrections_Two.js

--- Exam_One_Test_Corrections_Two.js
+++ Exam_One_Test_Corrections_Two.js
@@ -1,38 +1,51 @@
+
 /**
- * Write a description of class TestCorrections here.
+ * Write a description of class TestCorrections1 here.
  *
- * @author (Stephanie)
- * @version (10/14/19)
+ * @author (your name)
+ * @version (a version number or a date)
  */
-public class TestCorrections
+public class TestCorrections1
 {
-  
- public TestCorrections()
-   {
-        
-   }
-  public void sampleMethod()
-  {
-   //#3
-   int y = (int)(Math.random()*11) + 10;
-   //11 = how many numbers there are and 10 = where is starts
-   
-   //#4
-   int num = 4;
-   if (num/3 <= 1){
-       System.out.print("true");
+    // instance variables - replace the example below with your own
+    private int x;
+    private int sum;
+    public TestCorrections1()
+    {
+        sampleMethod();
     }
-   System.out.println("false");
-   //will return truefalse because num/3 will get truncated.
-   
-   //#9
-   for(int i = 0; i < 4; i++){
-       for(int j = 0; j <= i; j++){
-           System.out.print("X ");
+
+    public void sampleMethod()
+    {
+        //#3
+        int y = (int)(Math.random()*11) + 10;
+        System.out.print(y);
+
+        //11 = how many numbers there are and 10 = where is starts
+        //#4
+        int num = 4;
+        if (num/3 <= 1){
+            System.out.print("true");
         }
-       System.out.println();
+        System.out.println("false");
+        //will return truefalse because num/3 will get truncated
+
+        //#9
+        for(int i = 0; i < 4; i++){
+
+        }
+        //will return 
+
     }
-   //will return 
-   
-}
+
+    public void digitsSum(int num){
+        while (num%10 =! 0){
+            sum = sum + num%10;
+            num = num/10;
+        }
+    }
+        public static void main(){
+            TestCorrections1 Tc1 = new TestCorrections1();
+        }
+    }
 }
