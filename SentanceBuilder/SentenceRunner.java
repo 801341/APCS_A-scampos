6f79a47fd357407d046751b491e5c0a97c4e0946
801341 <801341@users.noreply.github.com>
2019-10-21 10:35:38 -0700

    10/24

---
+++ SentanceBuilder/SentenceRunner.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class SentenceRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SentenceRunner
+{
+    // instance variables - replace the example below with your own
+    /**
+     * Constructor for objects of class SentenceRunner
+     */
+    public SentenceRunner()
+    {
+    Sentencerunner
+    }
+}
---
+++ sort/BubbleSort.java
@@ -0,0 +1,26 @@
+
+/**
+ * Will recreate BubbleSort.
+ *
+ * @author (Stephanie)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class BubbleSort
+{
+    private ArrayList<Integer> numList = new ArrayList<Integer>();
+    public BubbleSort()
+    {
+    
+    }
+    public void loadNumbers(int n)
+    {
+        int nums = (int) (Math.random() * 100) + 1;
+        
+    }
+    public  void bubbleSort(){
+        for (int i = numList.size() - 1; i > 0; i--){
+            
+        }
+    }
+}
