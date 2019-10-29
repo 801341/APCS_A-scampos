93cd945948155bbfcf3e502c5c2030e061417278
801341 <801341@users.noreply.github.com>
2019-10-29 10:29:53 -0700

    s

---
+++ StudentList/StudList.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class StudList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class StudList
+{
+    ArrayList<Student> StudentList;
+    private String fName;
+    private String mName;
+    private String lName;
+    private int stuNumber;
+    private double gpa;
+    public StudList()
+    {
+      
+    }
+    public void add(Students){
+        
+    
+    }
+    public void deleteStudent(String LastName)
+    {
+      
+    }
+    public void deleteStudent(int stuNumber)
+    {
+        
+    }
+    public void clearList()
+    {
+        
+    }
+    public void printList()
+    {
+      
+    }
+}
---
+++ StudentList/StudListRunner.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class StudListRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudListRunner
+{
+    private int x;
+    public StudListRunner()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public void menuNumber(userinput){
+        out.line.print("1. Add Student");
+        out.line.print("2. Delete Student");
+        out.line.print("3. Print Student records");
+        out.line.print("4. Search for student");
+        out.line.print("5. Delete Student");
+    }
+}
