bd91d8e4db4499ee94b8a54b1c32bdaf073b18b0
801341 <801341@users.noreply.github.com>
2019-11-08 10:10:35 -0800

    sc

--- Github/StudentList/StudListRunner.java
+++ Github/StudentList/StudListRunner.java
@@ -2,22 +2,22 @@
 /**
  * Write a description of class StudListRunner here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Stephanie)
+ * @version (11/8/2019)
  */
 public class StudListRunner
 {
-    private int x;
+    Studlist Stud = new Studlist();
     public StudListRunner()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-    public void menuNumber(userinput){
-        out.line.print("1. Add Student");
-        out.line.print("2. Delete Student");
-        out.line.print("3. Print Student records");
-        out.line.print("4. Search for student");
-        out.line.print("5. Delete Student");
+    public void menuNumber(int userinput){
+        System.out.print("1. Add Student");
+        System.out.print("2. Delete Student");
+        System.out.print("3. Print Student records");
+        System.out.print("4. Search for student");
+        System.out.print("5. Delete Student");
+        
     }
 }
